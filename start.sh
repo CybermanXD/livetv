aefcd3251d83366527b6f63701b5b2b6caabcf3c
CybermanXD <149437762+CybermanXD@users.noreply.github.com>
2025-03-16 09:22:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 chmod +x ./jiotv_go-linux-amd64
 
-# Start the server on localhost (where it's stuck)
+# Start the app (which binds to 127.0.0.1:5001)
 ./jiotv_go-linux-amd64 start &
 
-# Forward external traffic from 0.0.0.0:$PORT to internal 127.0.0.1:5001
-socat TCP-LISTEN:$PORT,fork TCP:127.0.0.1:5001
+# Forward Railway $PORT (external) to internal 127.0.0.1:5001
+socat TCP-LISTEN:${PORT},fork TCP:127.0.0.1:5001
